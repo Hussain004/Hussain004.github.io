0d510b3b52b52ed9f46cd17abe9fb519406ae4e4
Hussain004 <hussain04habib@gmail.com>
2024-07-18 13:23:48 +0500

    Update settings.js

--- F1-Game/js/settings.js
+++ F1-Game/js/settings.js
@@ -4,7 +4,7 @@ class Settings
         // reference to the game scene
         this.scene = scene;
 
-        var font = {font: '32px Arial', fill: '#000000'};
+        var font = {font: '16px Arial', fill: '#000000'};
         this.txtPause = scene.add.text(1720, 5, '', font);
 
         this.show();
