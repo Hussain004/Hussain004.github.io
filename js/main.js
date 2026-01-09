376c462d4011583f98d40d93d195e467b2268d22
Hussain004 <hussain04habib@gmail.com>
2026-01-09 07:25:30 +0500

    Removed preloader

--- js/main.js
+++ js/main.js
@@ -20,27 +20,14 @@
     // doc.setAttribute('data-useragent', navigator.userAgent);
 
 
-   /* preloader
+   /* Page Load Handler
     * -------------------------------------------------- */
-    const ssPreloader = function() {
-
-        $("html").addClass('ss-preload');
+    const ssPageLoad = function() {
 
         $WIN.on('load', function() {
-
-            // force page scroll position to top at page refresh
-            $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            }); 
-            
             // for hero content animations 
             $("html").removeClass('ss-preload');
             $("html").addClass('ss-loaded');
-
         });
     };
 
@@ -415,7 +402,7 @@
     * ------------------------------------------------------ */
     (function ssInit() {
 
-        ssPreloader();
+        ssPageLoad();
         ssPrettyPrint();
         ssMoveHeader();
         ssMobileMenu();
